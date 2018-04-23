9f327fb7b5e9b4a3f85e52a3d4fbd93aaa96acfb
unknown <stuartalafollette@gmail.com>
2018-04-22 20:01:06 -0500

    brief update with objects for character data

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,15 +6,32 @@ var charOneAtk = 10;
 var charTwoAtk = 20;
 var charThreeAtk= 30;
 var charFourAtk= 40;
-
-
-
-
-
-
+var charHealth;
+var defenderHealth;
+var obiWan = {
+    charHealth: 120,
+    charAtk: 30,
+    charCounter: 5,
+}
+var luke = {
+    charHealth: 100,
+    charAtk: 20,
+    charCounter: 10,
+}
+var obiWan = {
+    charHealth: 150,
+    charAtk: 10,
+    charCounter: 20,
+}
+var obiWan = {
+    charHealth: 180,
+    charAtk: 5,
+    charCounter: 25,
+}
 $(".char-choices").on("click", "figure", function(){
     chosenChar = this;
     $(".your-char").append(chosenChar);
+    parseInt(chosenChar)
     otherChar = $(".char-choices .char");
     attackers()
 });
@@ -31,5 +48,14 @@ $(".defenders-left").on("click", "figure", function(){
     }
     else{
         return false;
+    }
+});
+
+$(".attack").on("click", function(){
+    if (!defender){
+        return false;
+    }
+    else {
+        console.log("it works yo")
     }
 });
