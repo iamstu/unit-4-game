278a31e0f9a68e605814e1ab4ac9e9dd36020e6a
unknown <stuartalafollette@gmail.com>
2018-04-27 20:37:23 -0500

    final commit before submission hopefully

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 var chosenChar;
 var otherChar;
 var defender;
+//array to find the location of character data by id of html element
 var charNameArray = ["obiWan", "Luke", "Sidius", "Maul"];
+//array to acces data and use with correct character
 var characterDataArray = [
     obiWan = {
         charHealth: 120,
@@ -24,9 +26,12 @@ var characterDataArray = [
         charCounter: 30,
 }];
 var chosenStats;
+//var to make atk increase static
 var atkUp;
 var defenderStats;
+//array to determin if win scenario is met
 var defeatedArray= [];
+//our reset function
  function initializeGame() {
         $(".char-choices").append($("#obiWan"));
         $(".char-choices").append($("#Luke"));
@@ -66,10 +71,11 @@ var defeatedArray= [];
             $("#Sidius").find(".health").text(characterDataArray[2].charHealth);
             $("#Maul").find(".health").text(characterDataArray[3].charHealth)
         }
-   
+   //sets the clicked figure to the chosen character var and moves it to the correct section
     $(".char-choices").on("click", "figure", function(){
     chosenChar = this;
     $(".your-char").append(chosenChar);
+    //a loop to find the data for the clicked char by the id of the character and setting the atk increase
     for (var i = 0; i < charNameArray.length; i++){
         if ($(chosenChar).attr("id") === charNameArray[i]){
             chosenStats = characterDataArray[i];
@@ -77,7 +83,7 @@ var defeatedArray= [];
             console.log(chosenStats);
         }
     }
-    parseInt(chosenChar)
+    //moves the other imgs to the appropriate div after eveything else is done
     otherChar = $(".char-choices .char");
     attackers()
 });
@@ -87,11 +93,12 @@ function attackers(){
 }
 
 $(".defenders-left").on("click", "figure", function(){
-    
+    //make sure the defender area is empty and then allow player to chose defender
     if (!defender){ 
         defender = this;
         $().addClass($("current-defender"));
     $(".defender").append(defender);
+    //same loop for player character but for defender
     for (var i = 0; i < charNameArray.length; i++){
         if ($(defender).attr("id") === charNameArray[i]){
             defenderStats = characterDataArray[i];
@@ -105,17 +112,20 @@ $(".defenders-left").on("click", "figure", function(){
 });
 
 $(".attack").on("click", function(){
-
+//prevent idle atk from breaking my hard work
     if (!defender){
         return false;
     }
+    //decrease health of each and print that to screen
     else {
         chosenStats.charHealth -= defenderStats.charCounter;
         $(chosenChar).find(".health").text(chosenStats.charHealth);
         defenderStats.charHealth -= chosenStats.charAtk;
         $(defender).find(".health").text(defenderStats.charHealth);
+        //after atk is done increase player atk
         chosenStats.charAtk += atkUp;
         console.log(chosenStats.charAtk);
+        //check to see if defender is alive
         if (defenderStats.charHealth <= 0){
             // append defender data to array to measure length - 1 for win scenario
             defeatedArray.push(defender);
@@ -126,6 +136,7 @@ $(".attack").on("click", function(){
                 initializeGame();
             }
         }
+        //make sure player is alive (it is assumed player atks first so player health is measured last)
         if (chosenStats.charHealth <= 0){
             initializeGame();
         }
